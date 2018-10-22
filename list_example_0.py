80adf38c8f4d23b72dd3ff0084f23ac1dab0a866
NathanHigley <nathan@jhigley.com>
2018-10-22 08:35:19 -0700

    that's not what a constellation is

---
+++ list_example_0.py
@@ -0,0 +1,10 @@
+#list_example_0.py nh
+filelist1 = ["IMG_001.png", "IMG_007.png", "IMG_011.png", "IMG_777.png"]
+print(filelist1)
+print(filelist1[3])
+print(filelist1[-3])
+print(filelist1[-1])
+"""
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+"""
