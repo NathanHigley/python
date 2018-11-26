1890fcd05a097f9aca31111a174e1b1f1d9a39ce
NathanHigley <nathan@jhigley.com>
2018-11-26 08:52:14 -0800

    if

--- minecraft-pi-python/coriolis.py
+++ minecraft-pi-python/coriolis.py
@@ -1,19 +1,17 @@
 from mcpi.minecraft import Minecraft
 
 '''
-MATERIAL
 air = 0
 stone = 1
-slab = 44
 obs = 49
 ice = 79
 glow = 89
-glowred = 246
 core = 247
 '''
 
 def init():
 	mc = Minecraft.create()
+	#mc = Minecraft.create("10.183.3._", 4711)
 	x,y,z = mc.player.getPos()
 	return mc
 	
@@ -21,18 +19,20 @@ def sector(mc, x , y, z):
 	import random
 	#clear area
 	mc.setBlocks(x+5, y+1, z+0, x+75, y+75, z+75, 0)
+
 	#create sector
 	mc.setBlocks(x+5, y+1, z+0, x+75, y+75, z+75, 49)
 	mc.setBlocks(x+6,y+2, z+1, x+74, y+74, z+74, 0)
+	
 	#create planet
 	n = 0
-	b = [79, 247]
+	b = [22, 79, 247]
 	while n <= 9:
 		mc.setBlocks(x+6+n, y+2+n, z+1+n, x+74-n, y+2+n, z+74-n, (random.choice(b)))
 		if n == 0:
 			mc.setBlocks(x+6, y+2, z+1, x+74, y+2, z+74, 89)
 		n = n + 1
-
+	
 def frame(mc, x, y, z):
 	#create frame
 	mc.setBlocks(x+0, y+0, z+0, x+30, y+30, z+30, 1)
@@ -176,8 +176,50 @@ def port(mc, x, y, z):
 	mc.setBlocks(x+6, y+23, z+0, x+5, y+23, z+26, 1)
 	mc.setBlocks(x+4, y+24, z+0, x+0, y+24, z+26, 1)
 
-#def relic(mc, x, y, z):
-	
+def relic(mc, x, y, z):
+	#create crater
+	n = 0
+	while n <= 9:
+		mc.setBlocks(x+50-n, y+11-n, z+50-n, x+25+n, y+11-n, z+25+n, 0)
+		n = n + 1
+		
+	#create relic
+	x = x + 22
+	y = y + 6
+	z = z + 38
+	mc.setBlocks(x+5, y+0, z+0, x+1, y+0, z+0, 42)
+	mc.setBlocks(x+5, y+1, z+0, x+4, y+1, z+0, 20)
+	mc.setBlocks(x+6, y+2, z+0, x+10, y+2, z+0, 20)
+	mc.setBlocks(x+11, y+2, z+0, x+11, y+2, z+0, 42)
+	mc.setBlocks(x+6, y+1, z+1, x+8, y+1, z+1, 35,11)
+	mc.setBlocks(x+6, y+1, z-1, x+8, y+1, z-1, 35,11)
+	mc.setBlocks(x+9, y+1, z+1, x+9, y+1, z+1, 42)
+	mc.setBlocks(x+9, y+1, z-1, x+9, y+1, z-1, 42)
+	mc.setBlocks(x+9, y+2, z+1, x+10, y+2, z+1, 35,11)
+	mc.setBlocks(x+9, y+2, z-1, x+10, y+2, z-1, 35,11)
+	mc.setBlocks(x+10, y+3, z+1, x+11, y+3, z+1, 35,11)
+	mc.setBlocks(x+10, y+3, z-1, x+11, y+3, z-1, 35,11)
+	mc.setBlocks(x+6, y-1, z+0, x+6, y-1, z+0, 42)
+	mc.setBlocks(x+6, y-1, z+1, x+7, y-1, z+1, 35,11)
+	mc.setBlocks(x+6, y-1, z-1, x+7, y-1, z-1, 35,11)
+	mc.setBlocks(x+8, y-2, z+1, x+10, y-2, z+1, 35,11)
+	mc.setBlocks(x+8, y-2, z-1, x+10, y-2, z-1, 35,11)
+	mc.setBlocks(x+6, y+0, z+1, x+6, y+0, z+1, 245)
+	mc.setBlocks(x+6, y+0, z-1, x+6, y+0, z-1, 245)
+	mc.setBlocks(x+7, y+0, z+1, x+7, y+0, z+1, 42)
+	mc.setBlocks(x+7, y+0, z-1, x+7, y+0, z-1, 42)
+	mc.setBlocks(x+9, y+0, z+1, x+9, y+0, z+1, 42)
+	mc.setBlocks(x+9, y+0, z-1, x+9, y+0, z-1, 42)
+	mc.setBlocks(x+8, y+0, z+2, x+10, y+0, z+2, 42)
+	mc.setBlocks(x+8, y+0, z-2, x+10, y+0, z-2, 42)
+	mc.setBlocks(x+11, y+0, z+3, x+13, y+0, z+3, 42)
+	mc.setBlocks(x+11, y+0, z-3, x+13, y+0, z-3, 42)
+	mc.setBlocks(x+14, y+0, z+4, x+15, y+0, z+4, 42)
+	mc.setBlocks(x+14, y+0, z-4, x+15, y+0, z-4, 42)
+	mc.setBlocks(x+16, y+0, z+5, x+17, y+0, z+5, 42)
+	mc.setBlocks(x+16, y+0, z-5, x+17, y+0, z-5, 42)
+	mc.setBlocks(x+9, y+0, z+0, x+9, y+0, z+0, 49)
+	mc.setBlocks(x+10, y+1, z+0, x+10, y+1, z+0, 35,1)
 	        
 def main():
 	mc = init()
@@ -186,6 +228,6 @@ def main():
 	sector(mc, 50, -15, 50)
 	frame(mc, 75, 20, 75)
 	port(mc, 88, 20, 78)
-	#relic()
+	relic(mc, 50, -15, 50)
 
 main()
